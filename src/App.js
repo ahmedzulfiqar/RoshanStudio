d9b92820c7c2fede391ce0d0593c5ff93e02c524
ahmedzulfiqar <ahmdzulfiqar01@example.com>
2024-05-12 11:20:59 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "./index.css";
 import Routerfile from "./components/routes/Routerfile";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App p-0" >
+    <div className="App p-0">
+      {loading && (
+        <div className="overlay-loader bg-main">
+          <div className="banter-loader">
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+            <div className="banter-loader__box"></div>
+          </div>
+        </div>
+      )}
       <div className="row m-0">
         <Routerfile />
         <div
-          className="absoluer position-absolute"
+          className="absolute position-absolute"
           style={{ top: "710vh" }}
         ></div>
       </div>
